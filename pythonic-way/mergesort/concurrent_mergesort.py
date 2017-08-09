d3189b4ce2be8adaaeb39109b9b728067abbe095
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-08-09 20:08:47 -0300

    proxy manager eh completamente inviavel

---
+++ pythonic-way/mergesort/concurrent_mergesort.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# Gabriel Fernandes
+
+
+from multiprocessing import Pool, Manager, Array
+#from numba import jit
+from time import time
+import random
+
+
+#@jit
+def sort(array, lbound=0, rbound=None):
+    if rbound == None:
+        rbound = len(array)-1
+
+    manager = Manager()
+    interprocess_list = manager.list(array)
+    process_pool = Pool()
+
+    if lbound < rbound:
+        i = lbound + 1
+        workers = []
+        while i <= rbound:
+            j = lbound
+            while j <= rbound-i:
+                workers.append(process_pool.apply_async(merge,
+                    args=(interprocess_list, j, j + i-1, min(j-1 + 2*i, rbound))))
+                j += 2*i
+            for p in workers:
+                p.wait()
+            i *= 2
+
+    array[:] = list(interprocess_list)
+    return array
+
+
+#@jit
+def merge(array, lbound, mid, rbound):
+    aux = array[lbound:rbound+1]
+    mid -= lbound
+    rbound -= lbound
+    i, j, k = 0, mid+1, lbound
+
+    while i <= mid and j <= rbound:
+        if aux[i] <= aux[j]:
+            array[k] = aux[i]
+            i += 1
+            k += 1
+        else:
+            array[k] = aux[j]
+            j += 1
+            k += 1
+
+    while i <= mid:
+        array[k] = aux[i]
+        i += 1
+        k += 1
+    while j <= rbound:
+        array[k] = aux[j]
+        j += 1
+        k += 1
+
+
+def test():
+    assert sort([4,3,2,1]) == [1,2,3,4]
+    assert sort([4,2,3,1]) == [1,2,3,4]
+    assert sort([4,5,3,2,1]) == [1,2,3,4,5]
+
+    arr = list(range(100))
+    for _ in arr:
+        tmp = arr[:]
+        random.shuffle(tmp)
+        assert id(arr) != id(tmp)
+        assert sort(tmp) == arr
+
+    return 'test pass!'
+
+
+def benchmark():
+    start = time()
+
+    list_comprehension = time()
+    arr = [i for i in range(10**2)]
+    list_comprehension = time() - list_comprehension
+
+    list_copy = time()
+    arr2 = arr[:]
+    list_copy = time() - list_copy
+
+    shuffle_time = time()
+    random.shuffle(arr)
+    shuffle_time = time() - shuffle_time
+
+    not_equal_time = time()
+    assert arr != arr2
+    not_equal_time = time() - not_equal_time
+
+    sort_time = time()
+    sort(arr)
+    sort_time = time() - sort_time
+
+    equal_time = time()
+    assert arr == arr2
+    equal_time = time() - equal_time
+
+    print('List comprehension: {:10.6f} secs'.format(list_comprehension))
+    print('List copy: {:19.6f} secs'.format(list_copy))
+    print('Shuffle time: {:16.6f} secs'.format(shuffle_time))
+    print('Not equal time: {:14.6f} secs'.format(not_equal_time))
+    print('\033[1;36m', end='')
+    print('Sort time: {:19.6f} secs'.format(sort_time))
+    print('\033[0;0m', end='')
+    print('Equal time: {:18.6f} secs'.format(equal_time))
+    print('\033[1;31m', end='')
+    print('Total time: {:18.6f} secs'.format(time()-start))
+
+
+if __name__=='__main__':
+    #test()
+    benchmark()
