ab59abb2781a4232779f9eeac4a309cc622c6977
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-08-12 14:50:07 -0300

    asyncio

---
+++ pythonic-way/asyncio_35_syntax.py
@@ -0,0 +1,21 @@
+# Com base em http://curio.readthedocs.org/en/latest/tutorial.html
+import asyncio
+from random import randint
+
+
+async def countdown(name, time):
+    while time > 0:
+        print(name, '==>', time)
+        await asyncio.sleep(1)
+        time -= 1
+
+
+loop = asyncio.get_event_loop()
+
+tasks = [
+    asyncio.ensure_future(countdown('A', randint(2, 6))),
+    asyncio.ensure_future(countdown('B', randint(2, 6)))
+]
+
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
